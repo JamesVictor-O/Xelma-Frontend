6c04651d578429f2adb82bb03f6da7a8b2d00db6
marvelous <donatusmarvelous09@gmail.com>
2026-01-30 10:12:02 -0800

    chore: project update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./components/ThemeProvider";
-import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Header from "./components/Header";
 import Leaderboard from "./components/Leaderboard";
 import RouteProgressBar from "./components/RouteProgressBar";
-import PredictionCard from "./components/PredictionCard";
+
 import { Toaster } from "sonner";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <ThemeProvider>
-      <Toaster position="bottom-right" />
       <RouteProgressBar />
       <Header />
       <main className="pt-24 lg:pt-32 px-4 lg:px-14 min-h-screen bg-[#FAFAFA] dark:bg-gray-900">
